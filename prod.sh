4027a8066e0d1518f093a3ad18a7619b125f22b2
Zem4ik <vladbullzv@gmail.com>
2019-01-24 10:44:08 +0300

    https added

--- prod.sh
+++ prod.sh
@@ -3,4 +3,4 @@
 docker stop prod
 docker rm prod
 ./gradlew docker
-docker run --name prod --network="host" -t -d ru.zem4ik.todo/spring-backend --server.port=80
+docker run --name prod --network="host" -t -d ru.zem4ik.todo/spring-backend --server.port=8483
