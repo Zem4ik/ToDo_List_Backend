2c14c3ae730378ec6a1de7c5b2565baacd0a9bf5
Zem4ik <vladbullzv@gmail.com>
2019-01-25 14:01:23 +0300

    bd fix

--- src/main/resources/db/postgres/schema.sql
+++ src/main/resources/db/postgres/schema.sql
@@ -3,19 +3,20 @@ begin transaction;
 create table users
 (
   id       bigserial primary key,
-  username varchar(50) not null check ( username <> '' ) unique,
+  username varchar(50)  not null check ( username <> '' ) unique,
   password varchar(100) not null,
   name     varchar(50),
   surname  varchar(50),
   email    varchar(50),
-  image    bytea
+  -- 4096 - maximum path length
+  image    varchar(4096)
 );
 
 create table lists
 (
   id   bigserial primary key,
   name varchar(50) not null,
-  icon bytea
+  icon varchar(4096)
 );
 
 create table user_lists
@@ -28,7 +29,7 @@ create table user_lists
 create table tasks
 (
   id          bigserial primary key,
-  listId      int references lists (id),
+  list_id     bigint references lists (id),
   title       varchar(50) not null,
   important   boolean,
   date        date,
@@ -38,8 +39,8 @@ create table tasks
 create table comments
 (
   id     bigserial primary key,
-  taskID int references tasks (id),
-  userID int references users (id),
+  taskID bigint references tasks (id),
+  userID bigint references users (id),
   text   text not null,
   date   date,
   time   time
